7649869561405515864dc20e372b5b1f43b4cda1
atapardea <atapardea@gmail.com>
2016-07-19 18:37:49 +0300

    third commit

--- ATPAgenda/src/Backend/Abonat.java
+++ ATPAgenda/src/Backend/Abonat.java
@@ -5,5 +5,20 @@ package Backend;
 public class Abonat {
     private String nume;
     private String prenume;
+    private long CNP;
+    private long nrTel;
+
+    public Abonat(String nume, String prenume, long CNP, long nrTel) {
+        this.nume = nume;
+        this.prenume = prenume;
+        this.CNP = CNP;
+        this.nrTel = nrTel;
+    }
+
+    
+    
+    
     
 }
+
+
